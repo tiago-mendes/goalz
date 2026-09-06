e8a099b3f4846e2aa66ec96f226330e986ef098f
Tiago Mendes <tiagoms.php@gmail.com>
2026-09-06 15:32:35 +0000

    Improved left menu

--- resources/views/layouts/app/sidebar.blade.php
+++ resources/views/layouts/app/sidebar.blade.php
@@ -11,15 +11,15 @@
             </flux:sidebar.header>
 
             <flux:sidebar.nav>
-                <flux:sidebar.group :heading="__('Platform')" class="grid">
+                <flux:sidebar.group class="grid">
                     <flux:sidebar.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>
                         {{ __('Dashboard') }}
                     </flux:sidebar.item>
                     <flux:sidebar.item icon="tag" :href="route('expense-categories.index')" :current="request()->routeIs('expense-categories.*')" wire:navigate>Categories</flux:sidebar.item>
-                    <flux:sidebar.item :href="route('monthly-income.index')" :current="request()->routeIs('monthly-income.*')" wire:navigate>Income</flux:sidebar.item>
-                    <flux:sidebar.item :href="route('fixed-expenses.index')" :current="request()->routeIs('fixed-expenses.*')" wire:navigate>Fixed Expenses</flux:sidebar.item>
+                    <flux:sidebar.item icon="banknotes" :href="route('monthly-income.index')" :current="request()->routeIs('monthly-income.*')" wire:navigate>Income</flux:sidebar.item>
+                    <flux:sidebar.item icon="calendar-days" :href="route('fixed-expenses.index')" :current="request()->routeIs('fixed-expenses.*')" wire:navigate>Fixed Expenses</flux:sidebar.item>
                     @can('manage-users')
-                        <flux:sidebar.item :href="route('admin.users.index')" :current="request()->routeIs('admin.*')" wire:navigate>Admin</flux:sidebar.item>
+                        <flux:sidebar.item icon="users" :href="route('admin.users.index')" :current="request()->routeIs('admin.*')" wire:navigate>Manage Users</flux:sidebar.item>
                     @endcan
                 </flux:sidebar.group>
             </flux:sidebar.nav>
