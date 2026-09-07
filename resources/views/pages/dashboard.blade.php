71cf02c756e048484ecc019c298bd6022d79140d
Tiago Mendes <tiagoms.php@gmail.com>
2026-09-07 03:40:59 +0000

    Added category icons to dashboard

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -131,7 +131,22 @@ new #[Title('Monthly Overview')] class extends Component {
                 @forelse ($this->plannedExpenses as $planned)
                     <tr wire:key="planned-{{ $selectedMonth }}-{{ $planned['expense']->id }}">
                         <td class="px-4 py-3">{{ $planned['expense']->name }}</td>
-                        <td class="px-4 py-3">{{ $planned['expense']->expenseCategory?->name ?? 'Category unavailable' }}</td>
+                        <td class="px-4 py-3">
+                            @if ($category = $planned['expense']->expenseCategory)
+                                <span class="inline-flex items-center gap-2">
+                                    <span
+                                        class="shrink-0"
+                                        style="color: {{ $category->safeColor() }}"
+                                        aria-hidden="true"
+                                    >
+                                        <flux:icon :name="$category->safeIcon()" class="size-5" />
+                                    </span>
+                                <span>{{ $category->name }}</span>
+                                </span>
+                            @else
+                                Category unavailable
+                            @endif
+                        </td>
                         <td class="whitespace-nowrap px-4 py-3"><time datetime="{{ $planned['date']->toDateString() }}">{{ $planned['date']->format('M j') }}</time></td>
                         <td class="whitespace-nowrap px-4 py-3 text-right tabular-nums">{{ auth()->user()->currency }} {{ $planned['expense']->amount }}</td>
                     </tr>
